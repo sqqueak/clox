88e07d1dbe844fd5e4424a0b2f18d5b845b2e8dc
Emily Yao <emily2004.yao@gmail.com>
2024-12-23 15:29:23 -0600

    free mem tweak

--- chunk.c
+++ chunk.c
@@ -28,8 +28,7 @@ void writeChunk(Chunk* chunk, uint8_t byte, int line) {
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code = GROW_ARRAY(
         uint8_t, chunk->code, oldCapacity, chunk->capacity);
-    chunk->lines = GROW_ARRAY(
-        uint8_t, chunk->code, oldCapacity, chunk->capacity);
+    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
   }
   // add new element to code
   chunk->code[chunk->count] = byte;
