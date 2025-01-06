e69749330606ef6e6a1f86403e94674c1e02b7d7
Emily Yao <emily2004.yao@gmail.com>
2025-01-06 14:05:06 -0600

    logical not

--- debug.c
+++ debug.c
@@ -53,6 +53,7 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     case OP_MULTIPLY: return simpleInstruction("OP_MULTIPLY", offset);
     case OP_DIVIDE:   return simpleInstruction("OP_DIVIDE", offset);
     
+    case OP_NOT:      return simpleInstruction("OP_NOT", offset);
     case OP_NEGATE:   return simpleInstruction("OP_NEGATE", offset);
     case OP_RETURN:   return simpleInstruction("OP_RETURN", offset);
     default:
